e84a5d28708330afe05d0f28fba32fa524ff30c5
yqlbu <yqlbu@bu.edu>
2023-03-28 07:45:32 +0000

    add .screenlayout

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --off --output HDMI-A-1 --off --output DisplayPort-0 --off --output DisplayPort-1 --mode 3840x2160 --pos 0x0 --rotate left --output DisplayPort-2 --mode 3840x2160 --pos 2160x0 --rotate normal --output DisplayPort-3 --mode 3840x2160 --pos 6000x0 --rotate left --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off
