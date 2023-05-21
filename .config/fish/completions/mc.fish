499a417a1a4809e1bc0a245fd6075692e4a31f87
yqlbu <yqlbu@bu.edu>
2023-05-21 23:02:54 +0800

    update mc completion

--- .config/fish/completions/mc.fish
+++ .config/fish/completions/mc.fish
@@ -3,7 +3,7 @@ function __complete_mc
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /home/kev/.linuxbrew/Cellar/minio-mc/20230323200304/bin/mc
+    /home/kev/.linuxbrew/Cellar/minio-mc/20230504181016/bin/mc
 end
 complete -f -c mc -a "(__complete_mc)"
 
