35ad0115064231fbf520c42a042deec7fc807373
yqlbu <yqlbu@bu.edu>
2023-05-27 10:51:36 +0800

    fix(nvim): fix nvim.telescrope ignore pattern

--- .config/nvim/lua/config/tools.lua
+++ .config/nvim/lua/config/tools.lua
@@ -29,7 +29,7 @@ function tools.setup()
 	require("telescope").setup({
 		defaults = {
 			defaults = require("telescope.themes").get_ivy(),
-			file_ignore_patterns = { "node_modules", ".git" },
+			file_ignore_patterns = { "node_modules/", ".git/" },
 		},
 		pickers = {
 			find_files = {
