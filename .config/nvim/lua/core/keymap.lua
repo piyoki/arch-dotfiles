b5a898dec348fcd24e961af913be273613c51f07
yqlbu <yqlbu@bu.edu>
2023-06-01 10:42:09 +0800

    update harpoon keymaps

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -107,8 +107,8 @@ function M.setup()
 
   -- Harpoon
   utils.map('n', 'mm', ":lua require('harpoon.mark').add_file()<CR>") -- (N) Mark current file in harpoon
-  utils.map('n', 'mp', ":lua require('harpoon.ui').nav_prev()<CR>") -- (N) Navigate to previous harpoon mark
-  utils.map('n', 'mn', ":lua require('harpoon.ui').nav_next()<CR>") -- (N) Navigate to next harpoon mark
+  utils.map('n', '<C-t>', ":lua require('harpoon.ui').nav_prev()<CR>") -- (N) Navigate to previous harpoon mark
+  utils.map('n', '<C-h>', ":lua require('harpoon.ui').nav_next()<CR>") -- (N) Navigate to next harpoon mark
   utils.map('n', '<LEADER>m', ":lua require('harpoon.ui').toggle_quick_menu()<CR>") -- (N) Open up harpoon menu
 
   -- TeleScope (Short)
