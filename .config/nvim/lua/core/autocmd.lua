7303a2bd7ff593ad358b7e833a2b53ff2fb36a27
yqlbu <yqlbu@bu.edu>
2023-06-03 10:17:58 +0800

    fix nvim conflicts

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -14,7 +14,7 @@ function autocmd.setup()
     [[
     augroup FormatAutogroup
       autocmd!
-      autocmd BufWritePost *.md,*.yaml,*.json,*.go,*.lua,*.sh,*.js,*.ts,*.py,*.rb,*.hcl,*.tf FormatWrite
+      autocmd BufWritePost *.yaml,*.json,*.go,*.lua,*.sh,*.js,*.ts,*.py,*.rb,*.hcl,*.tf FormatWrite
       autocmd BufNewFile,BufRead *.hcl set filetype=terraform syntax=terraform
     augroup END
   ]],
