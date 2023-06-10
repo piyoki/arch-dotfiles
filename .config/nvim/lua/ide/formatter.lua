4324976aae3fb9c9434c348a77bf474c5b9b163b
yqlbu <yqlbu@bu.edu>
2023-06-10 13:25:23 +0800

    add typescript formatter (prettier)

--- .config/nvim/lua/ide/formatter.lua
+++ .config/nvim/lua/ide/formatter.lua
@@ -116,6 +116,19 @@ function M.setup()
           }
         end,
       },
+      typescript = {
+        function()
+          return {
+            exe = 'prettier',
+            args = {
+              '--stdin-filepath',
+              vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+              '--double-quote',
+            },
+            stdin = true,
+          }
+        end,
+      },
       -- python formatter
       python = {
         function()
