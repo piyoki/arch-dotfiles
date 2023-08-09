72fbd7b4afc9c7e8c91b37b3130677c4667ff2b1
kev <yqlbu@bu.edu>
2023-08-09 21:35:01 +0800

    update nvim keymaps

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -50,21 +50,16 @@ function M.setup()
   -- Folding
   utils.map('n', '<C-z>', 'za', opts) -- (N) Fold current function
 
-  --- Undo
-  -- utils.map('n', '<C-c>', ':q<Esc>', opts)
+  -- System Clipboard [Copy/Paste/Cut]
+  -- Ref: https://vi.stackexchange.com/questions/84/how-can-i-copy-text-to-the-system-clipboard-from-vim
+  utils.map('n', '<LEADER>y', '"+yy', opts)
+  utils.map('v', '<LEADER>y', '"+y', opts)
+  utils.map('n', '<LEADER>x', '"+d', opts)
+  utils.map('v', '<LEADER>x', '"+dd', opts)
 
   --- Paste *(no delete on register)
   utils.map('n', '<LEADER>p', '"_dP', opts)
   utils.map('v', '<LEADER>p', '"_dP', opts)
-
-  -- System Clipboar [Copy/Paste/Cut]
-  -- Ref: https://vi.stackexchange.com/questions/84/how-can-i-copy-text-to-the-system-clipboard-from-vim
-  utils.map('n', '<LEADER>y', '"+yy', opts)
-  utils.map('v', '<LEADER>y', '"+y', opts)
-  utils.map('n', '<LEADER>p', '"+gP<esc>', opts)
-  utils.map('v', '<LEADER>p', 'd"+gP<esc>', opts)
-  utils.map('n', '<LEADER>x', '"+d', opts)
-  utils.map('v', '<LEADER>x', '"+dd', opts)
 
   ----------------------------------------------
   -- Explorer
