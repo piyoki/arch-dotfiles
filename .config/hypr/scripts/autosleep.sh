4851b79825958755cbe2cfcd1733b4b1d915d12b
kev <yqlbu@bu.edu>
2023-12-03 20:41:39 +0800

    update rofi configs

--- .config/hypr/scripts/autosleep.sh
+++ .config/hypr/scripts/autosleep.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+icon=/usr/share/icons/Papirus-Dark/symbolic/actions/system-suspend-symbolic.svg
+
+notify-send -i $icon 'Swayidle' 'sleep in 5s'
+
 swayidle -w \
   timeout 360 'hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on' \
