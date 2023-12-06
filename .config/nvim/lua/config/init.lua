09a5ee1aea03332e9b8424978574a63b25935435
kev <yqlbu@bu.edu>
2023-12-06 22:15:04 +0800

    migrate nvim

--- .config/nvim/lua/config/init.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.setup()
-  require('config.tools').setup()
-  require('config.interface').setup()
-end
-
-return M
--- .config/nvim/lua/ide/git.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.setup()
-  vim.g.gitblame_date_format = '%r'
-end
-
-return M
