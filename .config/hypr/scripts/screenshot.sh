ba6b3991b7c5dcd82338251636d1b865f2f106c8
kev <yqlbu@bu.edu>
2023-12-16 13:13:34 +0800

    patch(hypr): update screenshot script

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-class="string:x-canonical-private-synchronous:sys-notify"
-info_icon="/usr/share/icons/Papirus-Dark/48x48/status/dialog-information.svg"
-
-notify-send -h $class -u low -i $info_icon "Screenshot Capture" "\nSelect a region"
 grim -g "$(slurp)" - | convert - -shave 1x1 PNG:- | swappy -f -
